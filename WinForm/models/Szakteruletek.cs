796835cb892d256321ebf16032e5bc4cdbccaed2
Roland <csehi.rolcsi@gmail.com>
2021-12-29 16:36:09 +0100

    model create

---
+++ WinForm/models/Szakteruletek.cs
@@ -0,0 +1,8 @@
+﻿namespace WinForm.models
+{
+    public class Szakteruletek
+    {
+        private int szakterulet_id { get; set; }
+        private string megnevezes { get; set; }
+    }
+}
---
+++ WinForm/models/Szerelok.cs
@@ -0,0 +1,11 @@
+﻿namespace WinForm.models
+{
+    public class Szerelok
+    {
+        private int szerelo_id { get; set; }
+        private int szakterulet_id { get; set; }
+        private string nev { get; set; }
+        private string telefon { get; set; }
+        private int oradij { get; set; }
+    }
+}
