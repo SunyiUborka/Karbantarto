d80cbed25a372e05edc5890540550535fb705b01
SunyiUborka <csehi.rolcsi@gmail.com>
2021-12-29 23:16:39 +0100

    dbcontext create

---
+++ WinForm/KarbantartoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using WinForm.models;
+
+namespace WinForm;
+
+public class KarbantartoDbContext : DbContext
+{
+    public KarbantartoDbContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Szerelo> Szereloks { get; set; }
+    public DbSet<Megrendelo> Megrendeloks { get; set; }
+    public DbSet<Szakterulet> Szakteruleteks { get; set; }
+    public DbSet<Karbantartas> Karbantartasoks { get; set; }
+}
